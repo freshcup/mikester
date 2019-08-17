ab2c129c9d534ff1ac5f3bd56075c61f3a950b99
Mike Tracz <miketracz@hotmail.com>
2019-08-17 03:34:38 +0000

    debugging heroku

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,4 +1,7 @@
 class Routine < ApplicationRecord
+
+  belongs_to :user
+  
   validates :title, presence: true, length: { maximum: 90, minimum: 4 }
   validates :description, presence: true, length: { maximum: 90, minimum: 4 }
 end
